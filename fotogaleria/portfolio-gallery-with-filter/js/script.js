5f22a3162bc5e8ee6f8eaaeeb055406d909a01a7
jaroslavpotoc <132379473+jaroslavpotoc@users.noreply.github.com>
2023-05-18 19:54:56 +0200

    WEB DEVELOPER - GALLERY

--- fotogaleria/portfolio-gallery-with-filter/js/script.js
+++ fotogaleria/portfolio-gallery-with-filter/js/script.js
@@ -1,4 +1,5 @@
 filterSelection("all")
+
 function filterSelection(c) {
     var x, i;
     x = document.getElementsByClassName("column");
@@ -14,7 +15,9 @@ function w3AddClass(element, name) {
     arr1 = element.className.split(" ");
     arr2 = name.split(" ");
     for (i = 0; i < arr2.length; i++) {
-        if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
+        if (arr1.indexOf(arr2[i]) == -1) {
+            element.className += " " + arr2[i];
+        }
     }
 }
 
@@ -35,7 +38,7 @@ function w3RemoveClass(element, name) {
 var btnContainer = document.getElementById("myBtnContainer");
 var btns = btnContainer.getElementsByClassName("btn");
 for (var i = 0; i < btns.length; i++) {
-    btns[i].addEventListener("click", function(){
+    btns[i].addEventListener("click", function () {
         var current = document.getElementsByClassName("active");
         current[0].className = current[0].className.replace(" active", "");
         this.className += " active";
