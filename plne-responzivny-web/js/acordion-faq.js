772afdb0d419dc904f2cf696c5687d5042aff094
jaroslavpotoc <132379473+jaroslavpotoc@users.noreply.github.com>
2023-05-30 20:09:17 +0200

    JP GENERATION - SERVICES + FAQ - COMPLEAT

--- plne-responzivny-web/js/acordion-faq.js
+++ plne-responzivny-web/js/acordion-faq.js
@@ -1,6 +1,6 @@
 var acc = document.getElementsByClassName("accordion");
 var panels = document.getElementsByClassName("panel");
-var lastPanelIndex = acc.length - 1; // Index posledného panelu
+var lastPanelIndex = acc.length - 1;
 var i;
 
 for (i = 0; i < acc.length; i++) {
@@ -8,14 +8,12 @@ for (i = 0; i < acc.length; i++) {
         var panel = this.nextElementSibling;
         var isActive = panel.classList.contains("activefaq");
 
-        // Zatvorí všetky otvorené panely
         for (var j = 0; j < panels.length; j++) {
             panels[j].style.maxHeight = null;
             panels[j].classList.remove("activefaq");
             acc[j].classList.remove("activefaq");
         }
 
-        // Ak panel nie je aktívny, otvorí ho a pridá triedy "active"
         if (!isActive) {
             panel.style.maxHeight = panel.scrollHeight + "px";
             panel.classList.add("activefaq");
@@ -23,7 +21,6 @@ for (i = 0; i < acc.length; i++) {
         }
     });
 
-    // Aktivuje posledný panel pri načítaní stránky
     if (i === lastPanelIndex) {
         panels[i].style.maxHeight = panels[i].scrollHeight + "px";
         acc[i].classList.add("activefaq");
