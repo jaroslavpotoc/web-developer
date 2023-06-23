3d3e6618d6e5c5a172c3725f85870f77f8e1b7c7
jaroslavpotoc <132379473+jaroslavpotoc@users.noreply.github.com>
2023-06-23 02:33:06 +0200

    JP GENERATION - Animation site

--- plne-responzivny-web/js/fade-in-up-left.js
+++ plne-responzivny-web/js/fade-in-up-left.js
@@ -1,7 +1,7 @@
-const elements1 = document.querySelectorAll('.new-gen, .concept, .content1, .content2, .content-ser-1, .content-ser-2, .content-ser-3, .content-ser-4'); // Fade In Up
-const elements2 = document.querySelectorAll('.text, .text2, .con-head, .proj-head, .skill-head, .port-foto, .port-video, .headline-con, .con-look-ser, .con-look-ser-faq, .skill-text'); // Fade In Left
+const elements1 = document.querySelectorAll('.new-gen, .concept, .content1, .content2, .content-ser-1, .content-ser-2, .content-ser-3, .content-ser-4, .content-price-1, .content-price-2, .content-price-3, .content-price-4, .content-price-5, .content-price-6, .content-price-7, .content-price-8, .content-price-9, .who-container, .form-an'); // Fade In Up
+const elements2 = document.querySelectorAll('.text, .text2, .con-head, .proj-head, .skill-head, .port-foto, .port-video, .headline-con, .con-look-ser, .con-look-ser-faq, .skill-text, .zak-text, .con-look-conta, .services-conta'); // Fade In Left
 const elements3 = document.querySelectorAll('.photo-galery, .skill-img, .overlay-content, .logo-foot, .content-ser-line'); //Fade In
-const elements4 = document.querySelectorAll('.gallery-item, .video-column, .menu-1, .menu-2, .menu-3, .menu-4, .menu-5'); // Zoom In
+const elements4 = document.querySelectorAll('.gallery-item, .video-column, .menu-1, .menu-2, .menu-3, .menu-4, .menu-5, .container-about-col, .team-row, .photo-content, .contact, .form-zo'); // Zoom In
 
 elements1.forEach(function (element, index) {
     element.classList.add('fade-in-delay-' + index);
